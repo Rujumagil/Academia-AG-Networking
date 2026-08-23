135cd034c98d7da389e36c855f4d81a260c1a383
Rujumagil <proyectocompas.info@gmail.com>
2026-08-23 08:27:03 -0600

    Load open-access Utah lesson navigation v57

--- bootstrap.js
+++ bootstrap.js
@@ -78,7 +78,7 @@
       await loadScript('utah-module4-exam-v2.js?v=20260820.51');
       await loadScript('utah-module5-exam-v2.js?v=20260820.52');
       await loadScript('utah-module6-exam-v2.js?v=20260821.53');
-      await loadScript('utah-sequential-lock-v56.js?v=20260822.56');
+      await loadScript('utah-sequential-lock-v57.js?v=20260823.57');
       await loadScript('utah-clean-ui-v66.js?v=20260822.66');
       await loadScript('utah-course-images-v67.js?v=20260822.67');
       await loadScript('quiz-randomizer.js?v=20260817.12');
